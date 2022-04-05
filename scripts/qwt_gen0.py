ffd24da142b9ec4a0bca6d9d54b177fa8ab02b41
JoKeru <jokeru@users.noreply.github.com>
2022-04-05 01:12:22 +0100

    Update qwt_gen0.py

--- scripts/qwt_gen0.py
+++ scripts/qwt_gen0.py
@@ -22,11 +22,15 @@ url = f"https://api.elrond.com/accounts/{qowattrewards_addr}/tokens"
 response = requests.get(url)
 data = response.json()
 qowattrewards = int(data[0]['balance']) / pow(10, data[0]['decimals'])
-print(f"@qowattrewards.elrond = {qowattrewards:,.0f} qwt")
+print(f"@qowattrewards.elrond wallet = {qowattrewards:,.0f} qwt\n")
 
 # pull pre-mint contract data
+print(f"pulling down all pre-mint contract transactions, please be patient, this takes a bit of time ...")
 contract_addr = 'erd1qqqqqqqqqqqqqpgq0jdfyzt2afk3dc35y9s8q0t0qk2tc56a83gq6ftr00'
 minted = {}
+level = 1
+used = 0
+i = 0
 for tier in tiers:
   hex = f"mint_{tier}_nft".encode('utf-8').hex()
   b64 = base64.b64encode(hex.encode('utf-8')).decode('utf-8').replace("=", "")
@@ -40,18 +44,14 @@ for tier in tiers:
     print(f"error on the '{tier}' tier")
   data = response.text
   minted[tier] = int(data)
-# do the math
-level = 1
-gone = 0
-for i, tier in enumerate(tiers):
   ratio = minted[tier] * 100 / seats[i]
-  print(f"{tier}: minted {minted[tier]} seats out of {seats[i]} ({ratio:.2f}%)")
-  gone += minted[tier] * (10000 * level)
-  level = level * 3
-print(f"qwt used {gone:,.0f}")
-rewards = gone * 0.7 # 70% goes towards the rewards wallet
-print(f"reward bag: {rewards:,.0f} qwt")
+  print(f"- {tier} tier: minted {minted[tier]} seats out of {seats[i]} ({ratio:.2f}%)")
+  used += minted[tier] * (10000 * level)
+  i += 1
+  level *= 3
+print(f"total qwt used = {used:,.0f}")
 
 # validate run
+rewards = used * 0.7 # 70% goes towards the rewards wallet
 if qowattrewards != rewards :
   print(f"\nHouston, we have a problem:\n{qowattrewards} != {rewards}")
